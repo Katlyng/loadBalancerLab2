96631652663aba4a4862c9e5929714ab3907bff3
Katlyng <katlyn.galvis@uptc.edu.co>
2025-03-23 18:22:49 -0500

    json nodes-config

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const fs = require('fs');
+
+const loadNodesConfig = () => {
+    const rawData = fs.readFileSync('nodes.config.json');
+    return JSON.parse(rawData).nodes;
+};
+
+const nodes = loadNodesConfig();
+console.log("Nodos disponibles:", nodes);
 
 // Middleware para parsear JSON
 app.use(express.json());
